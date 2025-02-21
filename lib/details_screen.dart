6f95dd7d94c5855b5309019cfdcb20572cebdbdf
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-02-21 13:44:25 -0500

    Added Basic Code to Home Screen and Detail Screen

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,1 +1,17 @@
 //Details Screen
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String recipe = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe),
+      ),
+      body: Center(
+        child: Text("Details for $recipe"),
+      ),
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,1 +1,38 @@
 //Home Screen
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final List<String> recipes = ["Pasta", "Tacos", "Butter Chicken", "Chicken Biryani"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Recipe Book"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {
+              Navigator.pushNamed(context, '/favorites');
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: recipes.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(recipes[index]),
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/details',
+                arguments: recipes[index],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
