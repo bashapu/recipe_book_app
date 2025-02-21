fcdf2f6397cefdc43c0a7a7c5818e69f7a1f6386
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-02-21 14:01:08 -0500

    Details Screen Updated

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  bool isFavorite = false;
+
   @override
   Widget build(BuildContext context) {
     final String recipe = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
         title: Text(recipe),
+        actions: [
+          IconButton(
+            icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              setState(() {
+                isFavorite = !isFavorite;
+              });
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text("Details for $recipe"),
